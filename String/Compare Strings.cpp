a18859af0837051434411803a1406358483da141
BenjaminLL <854623230@qq.com>
2017-11-03 19:15:17 -0400

    compare strings

---
+++ String/Compare Strings.cpp
@@ -0,0 +1,39 @@
+//
+//  Compare Strings.cpp
+//  
+//
+//  Created by Zhiyuan Song on 2017-11-03.
+//
+
+#include <iostream>
+using namespace std;
+
+bool compareStrings(string &A, string &B) {
+    // write your code here
+    map<int, int> collection;
+    int size_a = A.size();
+    int size_b = B.size();
+    
+    if (size_b > size_a) return false;
+    
+    for (int i = 0; i < size_a; ++i) {
+        collection[A[i]] += 1;
+        if (i < size_b)
+            collection[B[i]] -= 1;
+    }
+    
+    for (map<int,int>::iterator it = collection.begin(); it != collection.end(); ++it) {
+        if (it->second < 0) return false;
+    }
+    
+    return true;
+}
+
+int main() {
+    string a;
+    string b;
+    cin << a << b;
+    
+    bool result = compareStrings(a,b);
+    cout << result << endl;
+}
